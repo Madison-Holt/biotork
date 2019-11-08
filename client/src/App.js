7246bdb4defa6e65ee02345aeb72b9699d2911f4
cooliomadi <32204339+cooliomadi@users.noreply.github.com>
2019-11-08 16:18:29 -0500

    first and backend

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Route, Switch, Redirect  } from 'react-router-dom';
+import Home from "./views/Home/Home"
+import NotFound from "./views/NotFound"
+import Header from "./components/Header/Header"
+
+
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        // Render the newly fetched data inside of this.state.data 
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
+  /*
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/">
+          <Redirect to="/Home" />
+        </Route>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  );
+  */
+}
+
+export default App;
